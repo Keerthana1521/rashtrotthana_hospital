311fa01da118b83634c2c02dc55ce117215d15d7
keerthana <keerthana.m@iamneo.ai>
2024-06-28 09:38:11 +0530

    donation-page-done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { FacilitiesComponent } from './facilities/facilities.component';
 import { HealthCheckupComponent } from './health-checkup/health-checkup.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { DoctorLayoutComponent } from './doctor-layout/doctor-layout.component';
+import { DonationComponent } from './donation/donation.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'health-checkup', component: HealthCheckupComponent },
   { path: 'contact', component: ContactFormComponent },
   {path: 'doctor', component:DoctorLayoutComponent},
+  {path:'donation', component:DonationComponent}
 ]; 
 
 @NgModule({
