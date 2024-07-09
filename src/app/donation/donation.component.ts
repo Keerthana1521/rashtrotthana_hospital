e2b9fca1dc754bdf7f11b23be1cea340fdd01035
keerthana <keerthana.m@iamneo.ai>
2024-07-09 18:32:09 +0530

    'aos-bug-fixes'

--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-donation',
   templateUrl: './donation.component.html',
   styleUrl: './donation.component.css'
 })
-export class DonationComponent {
 
+export class DonationComponent implements OnInit {
+  ngOnInit() {
+    AOS.init();
+  }
 }
