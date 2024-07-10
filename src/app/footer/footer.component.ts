87a54a5bf594be8d3fabb98289e3efcebd454e49
keerthana <keerthana.m@iamneo.ai>
2024-07-10 15:48:19 +0530

    'socialmedialinks'

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  openLink(type: string) {
+    if (type == 'youtube') {
+      window.open('https://www.youtube.com/channel/UC5yBntegcZ_lR_IpfkbSVrA', '_blank');
+    }
+    else if (type == 'facebook') {
+      window.open('https://www.facebook.com/rashtrottanahospital', '_blank');
+    }
+    else if (type == 'twitter') {
+      window.open('https://x.com/Jmrhospital', '_blank');
+    }
+    else if (type == 'instagram') {
+      window.open('https://www.instagram.com/rashtrotthanahospital/?fbclid=IwZXh0bgNhZW0CMTEAAR2QboDfEwBQxDe7jIHuAmE-Syn3Y6kRp1NQS6KX71uMpbFO7H-JVHbfYBw_aem_akYFom-c4Mism2XS8h6G-w', '_blank');
+    }
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,16 +13,16 @@ export class NavbarComponent {
 
   openLink(type: string) {
     if (type == 'youtube') {
-      window.open('https://www.youtube.com/', '_blank');
+      window.open('https://www.youtube.com/channel/UC5yBntegcZ_lR_IpfkbSVrA', '_blank');
     }
     else if (type == 'facebook') {
-      window.open('https://www.facebook.com/', '_blank');
+      window.open('https://www.facebook.com/rashtrottanahospital', '_blank');
     }
     else if (type == 'twitter') {
-      window.open('https://twitter.com/', '_blank');
+      window.open('https://x.com/Jmrhospital', '_blank');
     }
     else if (type == 'instagram') {
-      window.open('https://www.instagram.com/', '_blank');
+      window.open('https://www.instagram.com/rashtrotthanahospital/?fbclid=IwZXh0bgNhZW0CMTEAAR2QboDfEwBQxDe7jIHuAmE-Syn3Y6kRp1NQS6KX71uMpbFO7H-JVHbfYBw_aem_akYFom-c4Mism2XS8h6G-w', '_blank');
     }
   }
 
