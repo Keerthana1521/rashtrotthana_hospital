39d83c071ac87372d47c61d2bce5353a1198a765
keerthana <keerthana.m@iamneo.ai>
2024-07-21 17:48:24 +0530

    'rithish-herina-proctology-testinomials'

---
+++ src/app/hernia/hernia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hernia',
+  templateUrl: './hernia.component.html',
+  styleUrl: './hernia.component.css'
+})
+export class HerniaComponent {
+
+}
---
+++ src/app/proctology/proctology.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proctology',
+  templateUrl: './proctology.component.html',
+  styleUrl: './proctology.component.css'
+})
+export class ProctologyComponent {
+
+}
