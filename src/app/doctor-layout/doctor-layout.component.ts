35fa61bfb072289c7bf6e79f9c0fbe96e610c5ab
keerthana <keerthana.m@iamneo.ai>
2024-07-27 14:28:38 +0530

    doctor-details

--- src/app/doctor-layout/doctor-layout.component.ts
+++ src/app/doctor-layout/doctor-layout.component.ts
@@ -68,30 +68,69 @@ contactForm:any = FormGroup;
 'Vaginal Surgeries, Management of menopause related issues',]
       },
       {
-        name:'Dr. Atmaram D C',
-        image: 'assets/doctor-3.png',
-        desgination: 'SURGERY/LAPROSCOPY/GASTR OENTEROLOGY',
-        about: 'Presently working as Assistant Professor Neurosurgery at Dayanand Sagar Medical College, Kanakpura Road.',
+        name:'Dr. Shekar Patil',
+        image: 'assets/doctor-9.png',
+        desgination: 'SR. CONSULTANT MEDICAL ONCOLOGIST',
+        about: 'Dr. Shekar Patil is a Medical Oncologist Bangalore. He has been practising medicine for 39 years including over 25 years in medical oncology. He treats conventional and high ...',
         speciality: 'General Medicine',
-        areasOfExpertise: ['GI surgery', 'Laproscopy', 'Onco Surgery', 'Lasers' ]
+        areasOfExpertise: ['All adult cancers with special interest in precision oncolgy','immuno-oncology' ]
       },
-
-{
-        name:'Dr. Rajeev Vijayakumar',
-        image: 'assets/doctor-4.png',
-        desgination: 'Oncologist',
-        about: 'Dr Rajeev, the very senior consultant whose expertise in Medical Oncology, Hemato Oncology, and Bone Marrow Transplant ensures that your confidence in his ...',
+      {
+        name:'Dr. RAVISHANKAR. D',
+        image: 'assets/doctor-6.png',
+        desgination: 'Orthopedic surgeon',
+        about: 'Dr. RAVISHANKAR. D is a Orthopedic surgeon and has an experience of 38 years in this field. He completed MBBS from Bangalore University, India in 1985 and DNB - Orthopedics ...',
         speciality: 'General Medicine',
-        areasOfExpertise: ['Medical Oncology', 'Hemato- Oncology', 'Bone Marrow Transplant' ]
+        areasOfExpertise: ['Joint Replacement Surgery', 'Hip Replacement', 'Fracture Treatment', 'Joint Dislocation Treatment' ]
       },
-
-{
+      {
+        name:'Dr. Pramod Cinder',
+        image: 'assets/doctor-26.png',
+        desgination: 'ORTHOPAEDIC ONCOSURGEON',
+        about: 'Dr. Shekar Patil is a Medical Oncologist Bangalore. He has been practising medicine for 39 years including over 25 years in medical oncology. He treats conventional and high ...',
+        speciality: 'General Medicine',
+        areasOfExpertise: ['Limb salvage surgeries with Biological reconstruction',
+'CATS ( Computer Assisted Tumour Surgery )', 
+'Use of Fresh Frozen Allograft',
+'Microsurgical reconstruction',
+'Complex Pelvic tumours and reconstructions for Chondrosarcomas, Osteosarcomas and Ewings sarcoma',
+'Use of molecular and targeted agents in the management of massive tumours (Denosumab in Giant cell tumours)',
+'Virtual Reality in Physiotherapy',
+'Robotic pelvic surgeries -Davinci',
+'3D Printing - Navigation in Oncology',
+'Stem cell and regenerative therapy' ]
+      },
+      {
+        name:'Dr. Nagaraj Rao',
+        image: 'assets/doctor-31.png',
+        desgination: 'UROLOGY',
+        about: '',
+        speciality: 'General Medicine',
+        areasOfExpertise: ['EndoUrology', 'Uro Oncology', 'Stone Disease' ]
+      },
+      {
+        name:'Dr. H M Krishnamurthy',
+        image: 'assets/doctor-49.png',
+        desgination: 'Consultant - Internal Medicine',
+        about: 'Dr. H.M.Krishnamurthy is a Doctor in Bangalore and has an experience of 40 years in this field. Dr. H.M.Krishnamurthy practices at Various hospitals in Bangalore and He completed ...',
+        speciality: 'General Medicine',
+        areasOfExpertise: ['Metabolic Disorders', 'Infectious Diseases', 'Geriatrics', 'Non Interventional- Respiratory Diseses', 'Diabetes- Thyroid', 'All Adult Chronic Diseases' ]
+      },
+      {
         name:'Dr. Shashi Vadhanan',
         image: 'assets/doctor-5.png',
         desgination: 'GENERAL SURGERY',
-        about: 'Neurosurgeon in practice since 10.5 years. Working in the hospital & state of care health care industry. Skilled in Neurospinal surgical procedures, Clinical Research&, Medical ...',
         speciality: 'General Medicine',
-        areasOfExpertise: ['Minimally Invasive Spine Surgery', 'Spinal Deformity Correction', 'Spinal Cord Tumors', 'Cerebrovascular Surgery', 'Neurosurgery', 'Neuroendoscopy', 'Deep Brain Stimulation', 'Epilepsy Surgery', 'Paediatric Neurosurgery' ]}
+        areasOfExpertise: ['Minimally Invasive Spine Surgery', 'Spinal Deformity Correction', 'Spinal Cord Tumors', 'Cerebrovascular Surgery', 'Neurosurgery', 'Neuroendoscopy', 'Deep Brain Stimulation', 'Epilepsy Surgery', 'Paediatric Neurosurgery' ]
+      },
+      {
+        name:'Dr. Savinay S Kanchibail',
+        image: 'assets/doctor-11.png',
+        desgination: 'NIOR CONSULTANT PAEDIATRICS & NEONATOLOGY',
+        about: 'I am a senior consultant paediatrician & Neonatologist at Rashtrotthana Hospital. I have more than 20 years of experience in Paediatrician – having worked in various...',
+        speciality: 'General Medicine',
+        areasOfExpertise: ['ALLERGY', 'NUTRITION & NEWBORN CARE' ]
+      },
     ]
     constructor(private fb: FormBuilder, public dialog: MatDialog) {}
 
