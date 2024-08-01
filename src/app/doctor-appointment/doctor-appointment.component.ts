73a06de4eb707d3c9517ea49119a9b06c8e77f6b
keerthana <keerthana.m@iamneo.ai>
2024-08-01 23:42:55 +0530

    appointment-form

--- src/app/doctor-appointment/doctor-appointment.component.ts
+++ src/app/doctor-appointment/doctor-appointment.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit,Input,EventEmitter,Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 // import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
@@ -42,9 +43,9 @@ export class DoctorAppointmentComponent implements OnInit {
         name: ['', Validators.required],
         email: ['', [Validators.required, Validators.email]],
         contactNumber: ['', Validators.required],
-        subject: ['', Validators.required],
-        message: ['', Validators.required],
         time: ['', Validators.required],
+        message: ['', Validators.required],
+        date_appointment: ['', Validators.required],
       });
       if (this.selectedDoctor && this.selectedDoctor.time) {
         this.availableTimes = [{ name: this.selectedDoctor.time }];
@@ -60,11 +61,41 @@ setDateConstraints() {
     this.disabledDays = [];
   }
 }
+formatDate(date: Date): string {
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
 onSubmit(): void {
   if (this.contactForm.valid) {
-    
+    const dateObj = this.contactForm.value.date_appointment;
+    const appointmentDate = dateObj ? this.formatDate(new Date(dateObj)) : '';
+    const emailParams = {
+      doctorName: this.selectedDoctor.name,
+      doctorDesignation: this.selectedDoctor.desgination,
+      patientName: this.contactForm.value.name,
+      patientEmail: this.contactForm.value.email,
+      patientContact: this.contactForm.value.contactNumber,
+      appointmentTime: this.contactForm.value.time.name,
+      appointmentDate: appointmentDate,
+      message: this.contactForm.value.message
+    };
+console.log(emailParams);
+    // Send the email using EmailJS
+    emailjs.send('service_31j2bps', 'template_3y6n6nf', emailParams, 'FA6BtQ1y1R6FlGMcz')
+      .then((response: EmailJSResponseStatus) => {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('Appointment request sent successfully.');
+      }, (error) => {
+        console.log('FAILED...', error);
+        alert('Failed to send appointment request. Please try again later.');
+      });
+
+    this.close.emit();
   }
-}
+  }
+
 get f(): { [key: string]: AbstractControl } {
   return this.contactForm.controls;
 } 
