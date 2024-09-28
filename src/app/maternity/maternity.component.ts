e4419868d2f490df7916016080294ba477742cf5
keerthana <keerthana.m@iamneo.ai>
2024-09-28 15:57:11 +0530

    routing

--- src/app/maternity/maternity.component.ts
+++ src/app/maternity/maternity.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-maternity',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './maternity.component.css'
 })
 export class MaternityComponent {
+  constructor(private router: Router) { 
+
+  }
+  contact(){
+    this.router.navigate(['/contact-us-bangalore']);
+  }
     doctors = [
       {
         image : "assets/doctor-2.png",
