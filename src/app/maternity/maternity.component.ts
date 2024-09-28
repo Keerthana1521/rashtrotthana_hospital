9e51ddff3e0026d5596c3be8f478bf3b7cdc25c8
Rithish06 <rithish.manohar006@gmail.com>
2024-09-28 15:24:00 +0530

    maternity page

---
+++ src/app/maternity/maternity.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maternity',
+  templateUrl: './maternity.component.html',
+  styleUrl: './maternity.component.css'
+})
+export class MaternityComponent {
+    doctors = [
+      {
+        image : "assets/doctor-2.png",
+        name : "Dr. Latha Venkataram",
+        experience : "Years of Experience: 32"
+      },
+      {
+        image : "assets/doctor-10.png",
+        name : "Dr. Shreelakshmi G",
+        experience : "Years of Experience: 20"
+      },
+      {
+        image : "assets/doctor-12.png",
+        name : "Dr. Prakruthi",
+        experience : "Years of Experience: 16"
+      }
+    ]
+    doctors2 = [
+      {
+        image : "assets/doctor-13.png",
+        name : "Dr. Neelam Saraswat",
+        experience : "Years of Experience: 10"
+      },
+      {
+        image : "assets/doctor-15.png",
+        name : "Dr. Ashwitha Gundmi",
+        experience : "Years of Experience: 7+"
+      },
+      {
+        image : "assets/doctor-33.png",
+        name : "Dr. Vinita Udupa",
+        experience : "Years of Experience: 8"
+      }
+    ]
+}
