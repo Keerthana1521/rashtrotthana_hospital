ebc8014756898025126541d5a9f1375563de85cb
keerthana <keerthana.m@iamneo.ai>
2024-11-04 17:22:25 +0530

    doc

--- src/app/doctor-layout/doctor-layout.component.ts
+++ src/app/doctor-layout/doctor-layout.component.ts
@@ -750,7 +750,7 @@ export class DoctorLayoutComponent {
       expertise: 'Years of Experience: 20',
       qualification:'MD (Ayu) in Rasa Shastra and Bhaishajya Kalpana from SDM College, Udupi PG Dip (Yoga) Diploma in Medical Astrology',
       time:'09:00-09:20,09:20-09:40,09:40-10:00,10:20-10:40,10:40-11:00,11:00-11:20,11:20-11:40,11:40-12:00,12:00-12:20,12:20-12:40,12:40-13:00,13:00-13:20,13:20-13:40,13:40-14:00,14:00-14:20,14:20-14:40,14:40-15:00,15:00-15:20,15:20-15:40,15:40-16:00,16:00-16:20,16:20-16:40,16:40-17:00',
-      date:'Monday-Saturday'
+      date:'Monday-Saturday,Sunday'
     },
     {
       name: 'Dr. Venkatesh H. S',
